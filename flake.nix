9f0e0d91d019938fc9884776a9ae0c8f01ef76ce
Faez Shakil <faez.shakil@gmail.com>
2022-08-27 00:57:35 +0500

    yes

--- flake.nix
+++ flake.nix
@@ -1,76 +1,60 @@
 {
   description = "Astaghfirullah";
-    inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixops-plugged.url = "github:lukebfox/nixops-plugged";
-  inputs.sops-nix.url = "github:Mic92/sops-nix";
-  outputs = { self, nixpkgs, flake-utils, nixops-plugged, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        packageName = "astaghfirullah";
-        pkgs = import nixpkgs {
-          inherit system;
-          config = {
-            allowUnfree = true;
-            allowBroken = true;
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
+    haskell-flake.url = "github:srid/haskell-flake";
+
+    # Haskell overrides
+    ema.url = "github:srid/ema/master";
+    ema.flake = false;
+    tailwind-haskell.url = "github:srid/tailwind-haskell";
+    tailwind-haskell.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = inputs@{ self, nixpkgs, flake-parts, haskell-flake, ... }:
+    flake-parts.lib.mkFlake { inherit self; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [
+        haskell-flake.flakeModule
+      ];
+      perSystem = { self', inputs', pkgs, ... }: {
+        # "haskellProjects" comes from https://github.com/srid/haskell-flake
+        haskellProjects.default = {
+          root = ./.;
+          haskellPackages = pkgs.haskell.packages.ghc924;
+          buildTools = hp: {
+            inherit (pkgs)
+              treefmt
+              nixpkgs-fmt
+              foreman;
+            inherit (pkgs.haskellPackages)
+              cabal-fmt;
+            inherit (hp)
+              fourmolu;
+            inherit (inputs'.tailwind-haskell.packages)
+              tailwind;
+
+            # https://github.com/NixOS/nixpkgs/issues/140774 reoccurs in GHC 9.2
+            ghcid = pkgs.haskell.lib.overrideCabal hp.ghcid (drv: {
+              enableSeparateBinOutput = false;
+            });
+          };
+          source-overrides = {
+            inherit (inputs)
+              ema;
+          };
+          overrides = self: super: with pkgs.haskell.lib; {
+            inherit (inputs'.tailwind-haskell.packages)
+              tailwind;
+            type-errors-pretty = dontCheck (doJailbreak super.type-errors-pretty);
+            relude = dontCheck (self.callHackage "relude" "1.1.0.0" { }); # 1.1 not in nixpkgs yet 
+            retry = dontCheck super.retry; # For GHC 9.2.
+            streaming-commons = dontCheck super.streaming-commons; # Fails on darwin
+            http2 = dontCheck super.http2; # Fails on darwin
           };
         };
-        haskellPackages = pkgs.haskellPackages;
-        jailbreakUnbreak = pkg:
-          pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
-
-        inherit (pkgs.lib.trivial) pipe flip;
-        inherit (pkgs.lib.lists) optionals;
-
-        haskPackages = haskellPackages.override {
-          overrides = self: super: rec {
-            servant = self.callHackage "servant" "0.18.3" {};
-            servant-server = self.callHackage "servant-server" "0.18.3" {};
-            lucid-htmx = jailbreakUnbreak haskellPackages.lucid-htmx;
-            servant-htmx = jailbreakUnbreak haskellPackages.servant-htmx;
-            streamly-process = pkgs.haskell.lib.dontCheck (jailbreakUnbreak super.streamly-process);
-          };
-        };
-        shellDeps = with haskPackages; [
-          haskell-language-server
-          ghcid
-          cabal-install
-          nixops-plugged.defaultPackage.${system}
-        ];
-        project = returnShellEnv:
-          haskPackages.developPackage {
-            inherit returnShellEnv;
-            name = packageName;
-            root = ./.;
-            withHoogle = returnShellEnv;
-            modifier = drv:
-              let inherit (pkgs.haskell.lib) addBuildTools;
-              in
-              pipe drv
-                [
-                  # Transform the Haskell derivation (`drv`) here.
-                  (flip addBuildTools
-                    (optionals returnShellEnv shellDeps))
-                ];
-          };
-      in
-      {
-        packages.${packageName} = project false;
-
-        defaultPackage = self.packages.${system}.${packageName};
-        devShells = {
-          default = project true;
-        };
-        devShell = self.devShells.${system}.default;
-
-        # nixopsConfigurations.default = {
-        #   network.storage.legacy.databasefile = "./deployments.nixops";
-        #   network.description = domain;
-        #   network.enableRollback = true;
-        #   defaults.nixpkgs.pkgs = pkgsFor "x86_64-linux";
-        #   defaults._module.args = {
-        #     inherit domain;
-        #   };
-        # };
-      }
-    );
+        apps.tailwind.program = inputs'.tailwind-haskell.packages.tailwind;
+      };
+    };
 }
